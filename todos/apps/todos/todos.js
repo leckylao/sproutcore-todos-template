fe491539f56e4ce3127fac4dab746c8012521f3e
Lecky Lao <lecky.lao@idealian.net>
2011-03-18 17:33:37 +1100

    done

--- todos/apps/todos/todos.js
+++ todos/apps/todos/todos.js
@@ -29,7 +29,28 @@ Todos.todoListController = SC.ArrayController.create(
   createTodo: function(title) {
     var todo = Todos.Todo.create({ title: title });
     this.pushObject(todo);
-  }
+  },
+  
+  remaining: function() {
+    return this.filterProperty('isDone', false).get('length');
+  }.property('@each.isDone'),
+  
+  clearCompletedTodos: function() {
+    this.filterProperty('isDone', true).forEach(this.removeObject, this)
+},
+  
+  allAreDone: function() {
+    return this.get('length') && this.everyProperty('isDone', true);
+  }.property('@each.isDone'),
+  
+  allAreDone: function(key, value) {
+    if (value !== undefined) {
+      this.setEach('isDone', value);
+      return value;
+    } else {
+      return this.get('length') && this.everyProperty('isDone', true);
+    }
+  }.property('@each.isDone')
   
 });
 
@@ -61,4 +82,23 @@ Todos.todoListView = SC.TemplateCollectionView.create({
 
 Todos.CheckboxView = SC.TemplateView.extend(SC.CheckboxSupport, {
   valueBinding: '.parentView.content.isDone'
+});
+
+Todos.statsView = SC.TemplateView.create({
+  remainingBinding: 'Todos.todoListController.remaining',
+
+  displayRemaining: function() {
+    var remaining = this.get('remaining');
+    return remaining + (remaining === 1 ? " item" : " items");
+  }.property('remaining').cacheable()
+});
+
+Todos.clearCompletedView = SC.TemplateView.create({
+  mouseUp: function() {
+    Todos.todoListController.clearCompletedTodos();
+  }
+});
+
+Todos.markAllDoneView = SC.TemplateView.create(SC.CheckboxSupport, {
+  valueBinding: 'Todos.todoListController.allAreDone'
 });
--- todos/tmp/debug/build/static/todos/en/current/source/todos.js
+++ todos/tmp/debug/build/static/todos/en/current/source/todos.js
@@ -29,7 +29,28 @@ Todos.todoListController = SC.ArrayController.create(
   createTodo: function(title) {
     var todo = Todos.Todo.create({ title: title });
     this.pushObject(todo);
-  }
+  },
+  
+  remaining: function() {
+    return this.filterProperty('isDone', false).get('length');
+  }.property('@each.isDone'),
+  
+  clearCompletedTodos: function() {
+    this.filterProperty('isDone', true).forEach(this.removeObject, this)
+},
+  
+  allAreDone: function() {
+    return this.get('length') && this.everyProperty('isDone', true);
+  }.property('@each.isDone'),
+  
+  allAreDone: function(key, value) {
+    if (value !== undefined) {
+      this.setEach('isDone', value);
+      return value;
+    } else {
+      return this.get('length') && this.everyProperty('isDone', true);
+    }
+  }.property('@each.isDone')
   
 });
 
@@ -46,9 +67,38 @@ Todos.CreateTodoView = SC.TemplateView.create(SC.TextFieldSupport, {
 });
 
 Todos.todoListView = SC.TemplateCollectionView.create({
-  contentBinding: 'Todos.todoListController'
+  contentBinding: 'Todos.todoListController',
+  
+  itemView: SC.TemplateView.extend({
+    // Add the 'done' class to this view
+    // if the Todo object is marked isDone
+    isDoneDidChange: function() {
+      var isDone = this.getPath('content.isDone');
+
+      this.$().toggleClass('done', isDone);
+    }.observes('.content.isDone')
+  })
 });
 
 Todos.CheckboxView = SC.TemplateView.extend(SC.CheckboxSupport, {
   valueBinding: '.parentView.content.isDone'
+});
+
+Todos.statsView = SC.TemplateView.create({
+  remainingBinding: 'Todos.todoListController.remaining',
+
+  displayRemaining: function() {
+    var remaining = this.get('remaining');
+    return remaining + (remaining === 1 ? " item" : " items");
+  }.property('remaining').cacheable()
+});
+
+Todos.clearCompletedView = SC.TemplateView.create({
+  mouseUp: function() {
+    Todos.todoListController.clearCompletedTodos();
+  }
+});
+
+Todos.markAllDoneView = SC.TemplateView.create(SC.CheckboxSupport, {
+  valueBinding: 'Todos.todoListController.allAreDone'
 });; if ((typeof SC !== 'undefined') && SC && SC.Module && SC.Module.scriptDidLoad) SC.Module.scriptDidLoad('todos');
